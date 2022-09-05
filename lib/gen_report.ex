36d193fe5424da6b23448eb0b6c997ffd908b278
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2022-09-05 12:19:51 -0300

    Cria report com paralelismo

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -9,6 +9,18 @@ defmodule GenReport do
     |> Enum.reduce(initial_report(), fn line, report -> update_report_with_line(report, line) end)
   end
 
+  def build_from_many(arg) when not is_list(arg) do
+    {:error, "Invalid input, should be a list of strings"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(initial_report(), fn {:ok, report}, combined_report ->
+      combine_reports(report, combined_report)
+    end)
+  end
+
   defp update_report_with_line(report, [name, hours, _day, month, year]) do
     all_hours = Map.update(report["all_hours"], name, hours, &(&1 + hours))
 
@@ -41,6 +53,30 @@ defmodule GenReport do
 
   defp update_hours_per_year(hours_per_year, hours, year) do
     Map.update(hours_per_year, year, hours, &(&1 + hours))
+  end
+
+  defp combine_reports(report, combined_report) do
+    all_hours = merge_maps_by_sum(report["all_hours"], combined_report["all_hours"])
+
+    hours_per_month =
+      merge_nested_maps(report["hours_per_month"], combined_report["hours_per_month"])
+
+    hours_per_year =
+      merge_nested_maps(report["hours_per_year"], combined_report["hours_per_year"])
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
+  end
+
+  defp merge_nested_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> merge_maps_by_sum(value1, value2) end)
+  end
+
+  defp merge_maps_by_sum(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
 
   defp initial_report do
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -19,4 +19,22 @@ defmodule GenReportTest do
       assert response == {:error, "Insira o nome de um arquivo"}
     end
   end
+
+  describe "build_from_many/1" do
+    test "When passing filenames, returns a report from all of them" do
+      filenames = ["reports/part_1.csv", "reports/part_2.csv", "reports/part_3.csv"]
+
+      response = GenReport.build_from_many(filenames)
+
+      assert response == ReportFixture.build()
+    end
+
+    test "When given invalid input, returns an error" do
+      response = GenReport.build_from_many("banana")
+
+      expected_response = {:error, "Invalid input, should be a list of strings"}
+
+      assert response == expected_response
+    end
+  end
 end
